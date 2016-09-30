bb451456711333871000cba6ba2613ff79840a25
Ray <raysmithdev@gmail.com>
2016-09-30 12:04:44 -0700

    Added API call for user data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.css';
 import Intro from './Intro';
 import Header from './Header';
+import Loading from './Loading';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const url = 'http://api.randomuser.me/?results=5000';
+
+    fetch(url)
+    .then((response) => {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    })
+    .then((data) => {
+        this.setState({ loading: false })
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        {this.state.loading ? <Loading /> : <p>Finished Loading...</p>}
         <Header/>
         <Intro
           title="Welcome"
---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React, { PropTypes } from 'react'
+import Spinner from 'react-spinkit'
+
+const Loading = () => {
+  return (
+    <Spinner spinnerName='double-bounce' />
+  )
+}
+
+export default Loading
